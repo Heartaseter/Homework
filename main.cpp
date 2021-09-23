e65901943f52acad5a3b1ba5ee7510ac12ddc804
Heartaster <406321927@qq.com>
2021-09-23 19:29:24 +0800

    Update main.cpp
    adding 4level function

--- main.cpp
+++ main.cpp
@@ -5,17 +5,45 @@
 
 using namespace std;
 
+int keyWordsReturn(string address);
+
+int switchReturn(string address);
+
+void caseReturn(int *p_howMany, int howMany,string address);
+
+int if_Return(string address);
+
+int elseIfReturn(string address);
+
 int main() {
+    //string address = "D:\\CLionProjects\\EE308_Lab2\\test.c";
+    string address;
+    int level;
+    cout<< "please enter the file address : "<<endl;
+    cin>>address;
+    cout <<"please enter the level you want"<<endl;
+    cin>>level;
+
+
+    }
+
+}
+
+
+int keyWordsReturn(string address) {
     string s1, s2;
-    int total = 0;
+    int total;
     string keywords[30] = {"if", "else", "auto", "break", "case", "char", "const", "continue", "default",
                            "do", "enum", "extern", "float", "for", "goto", "int", "long", "register",
                            "return", "short", "signed", "sizeof", "static", "struct", "switch",
                            "typedef", "union", "void", "volatile", "while"};//有do，就不用考虑double关键字，有signed就不用考虑unsigned
     //32个字符串，第一版本便是让他们搜索，接下来可以通过分类索引来改进搜索的速度,有doubule便多了一个do，除此之外保留关键词会不会一行出现相同的两个(可以拆分成一个个单词)，开关次数过多
     for (int i = 0; i < 30; ++i) {
-        ifstream fin(R"(D:\CLionProjects\EE308_Lab2\test.c)");
+        ifstream fin(address);
         s1 = keywords[i];
+        if (!fin) {
+            cout << "文件不能打开" << endl;
+        }//检测c文件是否打开
         while (getline(fin, s2)) {
             if (s2.find(s1) < 2000) {//一般来说每行不会超过80个词汇，2000个字符宽度足够
                 total++;
@@ -23,5 +51,107 @@ int main() {
         }
         fin.close();
     }
-    cout <<"total num: " << total << endl;
+    return total;
+}
+
+int switchReturn(string address) {
+    string s1, s2;
+    int total;
+    ifstream fin(address);
+    s1 = "switch";
+    while (getline(fin, s2)) {
+        if (s2.find(s1) < 2000) {//一般来说每行不会超过80个词汇，2000个字符宽度足够
+            total++;
+        }
+    }
+    return total;
+}
+//利用指针传递数组来储存每个switch有几个case，以default作为结尾的识别标志，有些switch没有写default 则会出问题。
+//而且这段代码无法识别嵌套
+void caseReturn(int *p_howMany, int howMany,string address) {
+    string s1, s2, s3;
+    int i = 0;
+    *(p_howMany + i) = 0;
+    s1 = "default";
+    s3 = "case";
+    ifstream fin(address);
+    if (howMany > 0) {
+        while (getline(fin, s2)) {
+            if (s2.find(s3) < 2000) {
+                *(p_howMany + i) += 1;
+            } else if (s2.find(s1) < 2000) {
+                i++;
+            }
+        }
+    }
+}
+//一级一级读取，做两级
+int if_Return(string address) {
+    string s1, s2;
+    int ifCount = 0;
+    ifstream fin(address);
+    if (!fin) {
+        cout << "文件不能打开" << endl;
+    }//检测c文件是否打开
+    s1 = "    } else {";
+    while (getline(fin, s2)) {
+        if (s2.find(s1)<2000){
+            ifCount++;
+        }
+        if (s2.find("        else {")<2000){
+            ifCount++;
+        }
+    }
+    return ifCount;
+}
+
+int elseIfReturn(string address){
+    string s1, s2;
+    int elseifCount = 0;
+    s1 = "    if";
+    ifstream fin(address);
+    if (!fin) {
+        cout << "文件不能打开" << endl;
+    }//检测c文件是否打开
+    while (getline(fin, s2)){
+        if (s2.find(s1)<2000){
+            while (getline(fin,s2)){
+                if (s2.find("    } else if")<2000){
+                    while (getline(fin,s2)){
+                        if (s2.find("    } else")<2000){
+                            elseifCount++;
+                            break;
+                        }
+                        continue;
+                    }
+                }else{
+                    continue;
+                }
+            }
+        }
+    }
+    fin.close();
+    ifstream fin1(address);
+    if (!fin1) {
+        cout << "文件不能打开" << endl;
+    }//检测c文件是否打开
+    while (getline(fin1, s2)){
+        if (s2.find("        if")<2000){
+            while (getline(fin1,s2)){
+                if (s2.find("        else if")<2000){
+                    while (getline(fin1,s2)){
+                        if (s2.find("        else")<2000){
+                            elseifCount++;
+                            break;
+                        }
+                        continue;
+                    }
+                }else{
+                    continue;
+                }
+            }
+        }
+    }
+    fin1.close();
+    return elseifCount;
 }
